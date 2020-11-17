9ed215cf479bc1dad358200c0d88207be63f1bb2
OracleX <xueguanxiong@gmail.com>
2020-11-17 16:42:17 -0600

    platform loan comment

--- src/pages/platformLoan/index.js
+++ src/pages/platformLoan/index.js
@@ -1,6 +1,7 @@
 import React, {PureComponent} from 'react';
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom';
+//css
 import {
     PlatformLoanWrapper,
     Comment,
@@ -31,22 +32,28 @@ class PlatformLoan extends PureComponent {
     render() {
         return (
             <PlatformLoanWrapper>
+                {/*title*/}
                 <PlatformLoanTitle>
                     <div className='title'>平台贷款</div>
                     <div className='rec'/>
                 </PlatformLoanTitle>
+                {/*sub title*/}
                 <Comment><p>个人房屋抵押贷款是指申请人以自己的房屋产权作为抵押向银行申请贷款,贷款资金的用途可用作个人消费和运营。</p>
                     <p>个人房屋抵押贷款区别个人住房抵押贷款。而个人住房贷款是指银行向借款人发放的用于购买自用普通住房的贷款。借款人申请个人住房贷款时必须提供担保。</p></Comment>
+                {/*main area*/}
                 <LoanFlowChart>
+                    {/*flow chart*/}
                     <FlowChartMain>
                         {this.getFlowChartLeft()}
                         {this.getFlowChartMid()}
                         {this.getFlowChartRight()}
                     </FlowChartMain>
+                    {/*the image, right over flow chart*/}
                     <FlowChartImg>
                         <img src={loanPic} alt=""/>
                     </FlowChartImg>
                 </LoanFlowChart>
+                {/*bottom part additional info*/}
                 <AdditionInfo>
                     <AdditionInfoContainer>
                         <AdditionInfoContainerItem>
@@ -70,6 +77,7 @@ class PlatformLoan extends PureComponent {
             </PlatformLoanWrapper>
         )
     }
+    /* left 3 steps in flow chart */
     getFlowChartLeft() {
         return (
             <FLowCharLeft>
@@ -87,6 +95,7 @@ class PlatformLoan extends PureComponent {
             </FLowCharLeft>
         )
     }
+    /* mid in flow chart, only arrows*/
     getFlowChartMid() {
         return (
             <FlowChartMid>
@@ -95,6 +104,7 @@ class PlatformLoan extends PureComponent {
             </FlowChartMid>
         )
     }
+    /*arrow*/
     getMidItem() {
         return (
             <MidItem>
@@ -104,6 +114,7 @@ class PlatformLoan extends PureComponent {
             </MidItem>
         )
     }
+    /*right 2 steps in flow chart*/
     getFlowChartRight() {
         return (
             <FlowChartRight>
