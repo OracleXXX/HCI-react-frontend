d2c131cb0da9728bfd3c195833391ddedbf890cf
MY <mingy.wang@outlook.com>
2020-11-17 23:18:21 -0800

    Added dependencies and made some changes to ContactUs and OurTeam

---
+++ src/pages/contactUs/style.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const ContactUSTitle = styled.div`
+  width: 19.3rem;
+  height: 6.6rem;
+  margin: 0 auto;
+  & .title {
+    margin: 0 auto;
+    width: 19.2rem;
+    height: 4.7rem;  
+    font-family: SourceHanSansSC-Bold;
+    font-size: 3.2rem;
+    color: #333333;
+    text-align: center;
+    line-height: 4.7rem;
+  }
+  & .rec {
+    width: 5rem;
+    height: 0.3rem;
+    background-color: #2D84FF;
+    border-radius: 0.15rem;
+    margin: 1.6rem auto 0;
+    vertical-align: bottom;
+  
+  }
+`
