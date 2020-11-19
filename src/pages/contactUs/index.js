22097dad95f9f36c63348ae07a02017ee5a4b9b9
OracleX <xueguanxiong@gmail.com>
2020-11-19 11:04:12 -0600

    our team done

--- src/pages/contactUs/index.js
+++ src/pages/contactUs/index.js
@@ -4,21 +4,23 @@ import {withRouter} from 'react-router-dom';
 import AppHook from './Hook';
 import * as Constants from './store/constants';
 import pic from '../../statics/imgs/footer/footerPic.png';
+import contactUsImg from '../../statics/imgs/contactUs/contactUsPic.png';
 import {Form, Button} from 'react-bootstrap';
 
 import {ActivityTitle as ContactUsTitle} from "../oneStepService/activities/style";
 
 import {
-    ContactUsWrapper
+    ContactUsWrapper,
+    LocationContainer,
+    QRCodeContainer,
+    FormContainer
 } from './style';
 
 
 class ContactUs extends PureComponent {
     render() {
         const contacts = () => (
-            <div>
                 <AppHook columns={Constants.contactColumns} data={Constants.contactData}/>
-            </div>
         );
 
         return (
@@ -27,16 +29,15 @@ class ContactUs extends PureComponent {
                     <div className='title'>联系我们</div>
                     <div className='rec'/>
                 </ContactUsTitle>
+                <LocationContainer>
 
+                </LocationContainer>
+                <ContactUsTitle>
+                    <div className='title'>扫描二维码立即咨询</div>
+                    <div className='rec'/>
+                </ContactUsTitle>
+                <QRCodeContainer>
 
-                <div>
-                    <div>
-                        {'联系我们'}
-                        <div className='tableClass'>
-                            {contacts()}
-                        </div>
-                    </div>
-                    {'扫描二维码立即咨询'}
                     <div className='eqheight'>
                         <div>
                             <img src={pic} width="100" height="100"/>
@@ -51,64 +52,65 @@ class ContactUs extends PureComponent {
                             <img src={pic} width="100" height="100"/>
                         </div>
                     </div>
-                    <div>
-                        <Form>
-                            <Form.Group controlId="formBasicName">
-                                <Form.Label className='form-label'>姓名</Form.Label>
-                                <br/>
-                                <Form.Control required type="name" aria-describedby="nameInfo"/>
-                                <br/>
-                                <Form.Text id="nameInfo" muted>
-                                    {Constants.NAME_HELP_TEXT}
-                                </Form.Text>
-                            </Form.Group>
+                </QRCodeContainer>
+                <FormContainer>
+                    <img src={contactUsImg} alt="" className="top-right-img"/>
+                    <Form>
+                        <Form.Group controlId="formBasicName">
+                            <Form.Label className='form-label'>姓名</Form.Label>
+                            <br/>
+                            <Form.Control required type="name" aria-describedby="nameInfo"/>
+                            <br/>
+                            <Form.Text id="nameInfo" muted>
+                                {Constants.NAME_HELP_TEXT}
+                            </Form.Text>
+                        </Form.Group>
 
-                            <Form.Group controlId="formBasicEmail">
-                                <Form.Label>电子邮箱</Form.Label>
-                                <br/>
-                                <Form.Control required type="email" aria-describedby="emailInfo"/>
-                                <br/>
-                                <Form.Text id="emailInfo" muted className='help-text'>
-                                    {Constants.EMAIL_HELP_TEXT}
-                                </Form.Text>
-                            </Form.Group>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>电子邮箱</Form.Label>
+                            <br/>
+                            <Form.Control required type="email" aria-describedby="emailInfo"/>
+                            <br/>
+                            <Form.Text id="emailInfo" muted className='help-text'>
+                                {Constants.EMAIL_HELP_TEXT}
+                            </Form.Text>
+                        </Form.Group>
 
-                            <Form.Group controlId="formBasicPhoneNumber">
-                                <Form.Label>电话号码</Form.Label>
-                                <br/>
-                                <Form.Control type="name" aria-describedby="phone"/>
-                                <br/>
-                                <Form.Text id="phone" muted>
-                                    {Constants.PHONE_HELP_TEXT}
-                                </Form.Text>
-                            </Form.Group>
+                        <Form.Group controlId="formBasicPhoneNumber">
+                            <Form.Label>电话号码</Form.Label>
+                            <br/>
+                            <Form.Control type="name" aria-describedby="phone"/>
+                            <br/>
+                            <Form.Text id="phone" muted>
+                                {Constants.PHONE_HELP_TEXT}
+                            </Form.Text>
+                        </Form.Group>
 
-                            <Form.Group controlId="formBasicWechat">
-                                <Form.Label>微信号</Form.Label>
-                                <br/>
-                                <Form.Control type="text" aria-describedby="wechatInfo"/>
-                                <br/>
-                                <Form.Text id="wechatInfo" muted>
-                                    {Constants.WECHAT_HELP_TEXT}
-                                </Form.Text>
-                            </Form.Group>
+                        <Form.Group controlId="formBasicWechat">
+                            <Form.Label>微信号</Form.Label>
+                            <br/>
+                            <Form.Control type="text" aria-describedby="wechatInfo"/>
+                            <br/>
+                            <Form.Text id="wechatInfo" muted>
+                                {Constants.WECHAT_HELP_TEXT}
+                            </Form.Text>
+                        </Form.Group>
 
-                            <Form.Group controlId="formBasicComments">
-                                <Form.Label>您的留言</Form.Label>
-                                <br/>
-                                <Form.Control type="text" aria-describedby="commentsInfo"/>
-                                <br/>
-                                <Form.Text id="commentsInfo" muted>
-                                    {Constants.COMMENT_HELP_TEXT}
-                                </Form.Text>
-                            </Form.Group>
+                        <Form.Group controlId="formBasicComments">
+                            <Form.Label>您的留言</Form.Label>
+                            <br/>
+                            <Form.Control type="text" aria-describedby="commentsInfo"/>
+                            <br/>
+                            <Form.Text id="commentsInfo" muted>
+                                {Constants.COMMENT_HELP_TEXT}
+                            </Form.Text>
+                        </Form.Group>
 
-                            <Button variant="primary" type="submit">
-                                提交
-                            </Button>
-                        </Form>
-                    </div>
-                </div>
+                        <Button variant="primary" type="submit">
+                            提交
+                        </Button>
+                    </Form>
+                </FormContainer>
             </ContactUsWrapper>
         )
     }
--- src/pages/contactUs/style.js
+++ src/pages/contactUs/style.js
@@ -3,3 +3,37 @@ import styled from "styled-components";
 export const ContactUsWrapper = styled.div`
   width: calc(192rem - 17px);
 `;
+export const LocationContainer = styled.div`
+  width: 120rem;
+  height: 16.8rem;
+  background-color: #FFFFFF;
+  box-shadow: 0 10px 20px 0 rgba(227, 227, 227, 0.50);
+  border-radius: 0.8rem;
+  margin: 4rem auto 5.7rem;
+  
+`;
+
+export const QRCodeContainer = styled.div`
+  width: 120rem;
+  height: 37rem;
+  margin: 4rem auto;
+  box-shadow: 0 10px 20px 0 rgba(227, 227, 227, 0.50);
+  border-radius: 0.8rem;
+
+`;
+
+export const FormContainer = styled.div`
+  position: relative;
+  width: 120rem;
+  height: 104.7rem;
+  box-sizing: border-box;
+  border: 1px solid red;
+  margin: 14.1rem auto 9.1rem;
+  & .top-right-img {
+    position: absolute;
+    right: 0;
+    top: 2.9rem;
+    width: 46.4rem;
+    height: 35.4rem;
+  }
+`;
