81b3a490b435578f2bf3f919c450b4b34cf11155
OracleX <xueguanxiong@gmail.com>
2021-02-08 03:51:46 -0600

    proxy

--- src/pages/newProject/store/constants.js
+++ src/pages/newProject/store/constants.js
@@ -165,5 +165,5 @@ export const CHANGE_NEW_PROJECT_LIST = 'newProject/CHANGE_NEW_PROJECT_LIST';
 export const ADDR_ICON = addrIcon;
 export const MORE_INFO_ICON = moreInfoIcon;
 export const SEE_MORE_ICON = seeMoreIcon;
-export const PROXY_URL = "//52.34.204.235:8080/";
-export const PROXY_URL_IMAGES = "//52.34.204.235:8080/images";
+export const PROXY_URL = "http://52.34.204.235:8080/";
+export const PROXY_URL_IMAGES = "http://52.34.204.235:8080/images";
